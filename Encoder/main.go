6a5ae6cddd33608f08f43cbcc9d12bdf2f36c34f
HunterXIII <volkraftroman@gmail.com>
2026-01-09 14:22:29 +0300

    Решения

--- Encoder/main.go
+++ Encoder/main.go
@@ -11,11 +11,11 @@ func encryptWord(word string) string {
 	}
 
 	firstChar := word[:1]
-	rest := word[1:]
+	runes := []rune(word[1:])
 
-	runes := []rune(rest)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
+	n := len(runes)
+	for i := 0; i < n/2; i++ {
+		runes[i], runes[n-1-i] = runes[n-1-i], runes[i]
 	}
 
 	return firstChar + string(runes)
